f7664fb53684599de961470c707dfcea8d74d3e6
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-10-15 17:47:32 +0800

    Change the Switcher.js name to switcher.js

---
+++ src/js/switcher.js
@@ -0,0 +1,20 @@
+
+class Switcher {
+  switchImage(
+    currentClass,
+    current,
+    value,
+    noValue,
+    close,
+    open
+  ) {
+    if (currentClass === value) {
+      current.src = close;
+    }
+    if (currentClass === noValue) {
+      current.src = open;
+    }
+  };
+};
+
+export const switcher = new Switcher();
