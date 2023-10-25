a930eac7afd9a94dea788c64680d7cdd06f49033
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-10-25 16:24:57 +0800

    Listen for clicks to show its content on the main
    Use imported function to toggle the previous element that is shown to
    hide and toggle the element that is hidden to show.

---
+++ src/js/create_html_files/create_project_aside.js
@@ -0,0 +1,94 @@
+import { pusher } from '../create_project/arrayOfElementInStrings';
+import { elementToggler } from '../create_html_files/form_aside'
+import remove from '../../assets/remove_FILL0_wght400_GRAD0_opsz24.svg';
+
+class ListOfProject {
+
+  AppendListChildren(
+    projectName, 
+    dataKey
+  ) {
+
+    const li = document.createElement('li');
+    const h3 = document.createElement('h3');
+    let removeImg = new Image();
+    removeImg.src = remove;
+
+    removeImg.setAttribute(
+      'draggable',
+      'false'
+    );
+
+    li.classList.add('project-list');
+
+    h3.textContent = `${projectName}`;
+    
+    const elements = [
+      li,
+      h3,
+      removeImg,
+    ];
+
+    const elementsToListen = [
+      li,
+      h3,
+    ]
+    
+    elementsToListen.forEach((element) => {
+      
+      element.addEventListener(
+        'click',
+        (event) => {
+          const dataKey = event.target.getAttribute('data-key');
+
+          pusher.elementPusher(
+            undefined, 
+            dataKey
+          );
+
+          
+          const lastElement = pusher.lastValueFromArrayOfElementInStrings;
+          const previousElement = pusher.secondLastValueFromArrayOfElementInStrings;
+
+          const lastQueryElement = document.querySelector(`${lastElement}`);
+          const previousQueryElement = document.querySelector(`${previousElement}`);
+
+          console.log(event, previousQueryElement)
+          
+          elementToggler(
+            previousQueryElement,
+            lastQueryElement,
+          )
+        }
+      )
+    })
+
+    removeImg.addEventListener(
+      'click',
+      (event) => {
+        event.stopImmediatePropagation();
+        li.remove();
+      }
+    );
+
+    elements.forEach((element) => {
+      element.setAttribute(
+        'data-key',
+        `${dataKey}`
+      );
+    });
+
+    const listChildren = [
+      h3,
+      removeImg
+    ];
+
+    listChildren.forEach((element) => {
+      li.appendChild(element);
+    });
+
+    return li
+  };
+};
+
+export const ListOfProjectForDom = new ListOfProject();
