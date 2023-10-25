5d6b92e6d28294ddb2bbe3b33f4f849a259c6ae0
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-10-25 16:39:21 +0800

    Use it's method to push to this file's array var
    Declare a variable to store string value
    Create a method that recieve a classlist of a button if the classlist
    was expected it will push this value to the array.
    
    Create an if statement that recieve a value of a data attribute
    and then use template string to variable it then push this value to the
    array
    
    Use getter method to return the value from array without ever exporting
    the array values

---
+++ src/js/create_project/pusher.js
@@ -0,0 +1,44 @@
+
+const arrayOfElementInStrings = ['div[data-today]'];
+
+class Pusher {
+
+  elementPusher = function addAValueToTheArrayOfElement(
+    classListValue,
+    dataAttribute = 0
+  ) {
+    const todayButtonClassListValue = 'today_button';
+    const thisWeekButtonClassListValue = 'date_range_button';
+    const dataAttributeValue = `div[data-key="${dataAttribute}"]`;
+
+    switch (classListValue) {
+      case todayButtonClassListValue: {
+        return arrayOfElementInStrings.push('div[data-today]');
+      }
+      case thisWeekButtonClassListValue: {
+        return arrayOfElementInStrings.push('div[data-this-week]');
+      }
+      default: {
+
+        // do nothing
+        break;
+      }
+    };
+
+    if (dataAttribute) {
+      return arrayOfElementInStrings.push(dataAttributeValue);
+    }
+  };
+
+  get secondLastValueFromArrayOfElementInStrings() {
+    return arrayOfElementInStrings.slice(-2).reverse().slice(-1);
+  };
+
+  get lastValueFromArrayOfElementInStrings() {
+    return arrayOfElementInStrings.slice(-1);
+  };
+}
+;
+
+
+export const pusher = new Pusher();
