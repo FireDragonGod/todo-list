7828dd220ac6e45e13f661f7a742aefc750639ea
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-10-27 17:01:13 +0800

    Add event listener for today and this week button
    Edit the namings of imported files and functions
    Add more function for save button to save the form for task query and
    making sure that they have at least the minimum for project name
    countF Use this function to get data keys to get the last value of that
    form to avoid dealing with styles collisioning with other elements on
    the DOM

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
-import { count, projectArr } from './js/create_project';
-import { formf } from './js/form_aside';
-import { conditionAppending } from './js/AppendingChildrenWithConditional';
 import './assets/style.css';
+import { formf } from './js/create_project/Form';
+import { pusher } from './js/create_project/pusher';
+import { elementToggler } from './js/create_html_files/form_aside';
+import { count, gettingProjectArr } from './js/create_project/create_project';
+import { conditionAppending } from './js/create_project/conditional_append';
+import { today } from './js/create_html_files/today';
+import { thisWeek } from './js/create_html_files/this_week';
+import { mainContent } from './js/create_html_files/todo_with_form';
 
-const addProjectButton = document.querySelector('.add_project');
+const addProjectButton = document.querySelector('button.add_project_button');
 const form = document.querySelector('.formList');
 
 addProjectButton.addEventListener(
@@ -41,6 +46,43 @@ cancel.addEventListener(
   }
 );
 
+const todayButton = document.querySelector('button.today_button');
+const thisWeekButton = document.querySelector('button.date_range_button');
+const content = document.querySelector('#context_content');
+
+content.appendChild(
+  today.createHTMLElementsForToday(999),
+);
+
+content.appendChild(
+  thisWeek.createHTMLElementsForThisWeek(999),
+);
+
+const homeButtons = [
+  todayButton,
+  thisWeekButton,
+];
+
+homeButtons.forEach(element => {
+  
+  element.addEventListener(
+    'click',
+    (event) => {
+
+      const eventClasListValue = event.target.classList.value;
+      pusher.elementPusher(eventClasListValue);
+
+      const lastElement = document.querySelector(`${pusher.lastValueFromArrayOfElementInStrings}`);
+      const secondToTheLastElement = document.querySelector(`${pusher.secondLastValueFromArrayOfElementInStrings}`);
+
+      elementToggler(
+        lastElement,
+        secondToTheLastElement
+      );
+    }
+  );
+});
+
 const save = document.querySelector('.save');
 
 save.addEventListener(
@@ -52,6 +94,7 @@ save.addEventListener(
     const projects = document.querySelector('.projects');
     const inputLength = input.value.length;
     const eventClasList = event.target.classList.value;
+    const countF = count();
 
     formf.formToggling(
       event,
@@ -63,15 +106,24 @@ save.addEventListener(
 
     formf.formInputSave(
       input.value,
-      count(),
+      countF,
       inputLength
     );
 
     conditionAppending.appendding(
       projects,
-      projectArr,
+      gettingProjectArr(),
       input
-    )
+    );
+
+    conditionAppending.appenddingNew(
+      content,
+      mainContent.representProjectName(
+        input.value,
+        countF
+      ),
+      inputLength,
+    );
 
     input.value = '';
   }
