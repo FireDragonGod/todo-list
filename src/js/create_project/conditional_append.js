f4bd9afc00c43e278f20558f95cf713e2a7b98ff
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-11-21 16:28:05 +0800

    Remove dependencies move the rest to content js
    Move the rest content to js because dependencies such as
    js that creates html and js logic creates tightly coupled objects

--- src/js/create_project/conditional_append.js
+++ src/js/create_project/conditional_append.js
@@ -1,44 +1,34 @@
-import { projectLists } from "./create_project";
-import { ListOfProjectForDom } from '../create_html_files/create_project_aside'
-
 const  AppendingChildrenWithConditional = () => {
 
-  const appendding = function AppendIfObjectLengthIsGreaterThanOrEqualToOne(
+  const projectOnAside = (
     parent,
     objectLength,
     inputValue,
-  ) {
+    obj
+  ) => {
     let minimumInputValueLength = inputValue.value.length;
 
     if (
       objectLength >= 1
       && minimumInputValueLength >= 4
     ) {
-
-      
-      const projectName = projectLists.getProjectsItem(-1).projectName;
-      const projectIndex = projectLists.getProjectsItem(-1).projectIndex;
-      
-      parent.appendChild(ListOfProjectForDom.AppendListChildren(
-        projectName,
-        projectIndex,
-      ))
+      parent.appendChild(obj)
     };
   };
 
-  const appenddingNew = function appendObjectIfTheValueIsGreaterThanOrEqualToFour(
+  const projectContent = (
     parent,
     object,
     valueLength,
-  ) {
+  ) => {
     if (valueLength >= 4) {
       return parent.appendChild(object)
     }
   };
 
   return {
-    appendding,
-    appenddingNew,
+    projectOnAside,
+    projectContent,
   }
 };
 
