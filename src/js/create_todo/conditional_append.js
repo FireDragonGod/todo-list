2dd767fa5540c9b53a3f249cf514507e9f3251c2
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-11-23 15:52:09 +0800

    Remove all dependecies to use parameters

--- src/js/create_todo/conditional_append.js
+++ src/js/create_todo/conditional_append.js
@@ -1,50 +1,27 @@
-import { todoItem } from '../create_html_files/create_todo_item';
-import { todoLists } from '../create_todo/todo_create';
-import { colorConvert } from '../create_todo/todo_create';
-import { dates } from '../create_todo/todo_create'
-
 const AppendWithCondition = () => {
 
-  const appendIfConditionsWereSatisfied = function appendChildrenWithThePropertiesFromTodoList(
+  const appendIfConditionsWereSatisfied = (
     titleLength,
     parent, 
     preventElement,
     hideElement,
-  ) {
+    todoListsLength,
+    todoItem,
+  ) => {
     'use strict';
     if (
-      todoLists.getTodoListsLength() >= 1
+      todoListsLength >= 1
       && titleLength >= 4
     ) {
       preventElement.preventDefault();
       hideElement.close();
-
-      const todoListsProperty = todoLists.getTodoListsItem(-1);
-
-      const countValue = todoListsProperty.index;
-      const titleValue = todoListsProperty.title;
-      const descriptionValue = todoListsProperty.description;
-      const priorityValue = todoListsProperty.priority;
-      const dueDateValue = todoListsProperty.dueDate;
-
-      const colorBasedPriority = colorConvert.priorityConvert(priorityValue)
-      const formattedDate = dates.formatDate(dueDateValue);
-
-      parent.appendChild(todoItem.createTodoItem(
-        countValue,
-        colorBasedPriority,
-        titleValue,
-        formattedDate,
-        descriptionValue,
-        priorityValue,
-      ));
+      parent.appendChild(todoItem);
     }
   };
 
   return {
     appendIfConditionsWereSatisfied,
-  }
-
+  };
 };
 
 export const appendWithObjectLengthCondition = AppendWithCondition();
