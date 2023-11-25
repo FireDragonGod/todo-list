cd47d0aee17aaff24b2c308d2eab34e44beb9399
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-11-25 15:26:48 +0800

    Apply consistent style to create_project_aside

--- src/js/create_html_files/create_project_aside.js
+++ src/js/create_html_files/create_project_aside.js
@@ -1,7 +1,7 @@
-import { htmlElement } from '../create_project/pusher';
-import { elementToggler } from '../create_html_files/form_aside'
 import remove from '../../assets/img/remove_FILL0_wght400_GRAD0_opsz24.svg';
+import { htmlElement } from '../create_project/pusher';
 import { projectLists } from '../create_project/create_project';
+import { storingProjectsInLocalStorage } from '../local_storage_/local_storage_projects';
 
 class ListOfProject {
 
@@ -37,41 +37,12 @@ class ListOfProject {
     
     elementsToListen.forEach((element) => {
       
-      element.addEventListener(
-        'click',
-        (event) => {
-          const dataKey = event.target.getAttribute('data-key');
-
-          htmlElement.elementPusher(
-            undefined,
-            dataKey,
-          );
-
-          const lastElement = htmlElement.getLastElement();
-          const previousElement = htmlElement.getSecondToTheLastElement();
-
-          const lastQueryElement = document.querySelector(`${lastElement}`);
-          const previousQueryElement = document.querySelector(`${previousElement}`);
-          
-          elementToggler( 
-            previousQueryElement,
-            lastQueryElement,
-          );
-        }
-      )
-    })
-
-    removeImg.addEventListener(
-      'click',
-      (event) => {
-        event.stopImmediatePropagation();
-        const projectContent = document.querySelector(`div[data-key="${dataKey}"]`);
-        const blankContents = 'div[data-blank]';
+      element.addEventListener('click', (event) => {
+        const dataKey = event.target.getAttribute('data-key');
 
         htmlElement.elementPusher(
           undefined,
-          undefined,
-          blankContents,
+          dataKey,
         );
 
         const lastElement = htmlElement.getLastElement();
@@ -79,18 +50,38 @@ class ListOfProject {
 
         const lastQueryElement = document.querySelector(`${lastElement}`);
         const previousQueryElement = document.querySelector(`${previousElement}`);
+        
+        previousQueryElement.classList.toggle('hide');
+        lastQueryElement.classList.toggle('hide');
+      });
+    })
 
-        elementToggler( 
-          previousQueryElement,
-          lastQueryElement,
-        );
+    removeImg.addEventListener('click', (event) => {
+      event.stopImmediatePropagation();
+      const projectContent = document.querySelector(`div[data-key="${dataKey}"]`);
+      const blankContents = 'div[data-blank]';
 
-        li.remove();
-        projectContent.remove();
-        projectLists.dereferProjectItem(dataKey);
-        localStorage.setItem(`Project ${dataKey}`, null);
-      }
-    );
+      htmlElement.elementPusher(
+        undefined,
+        undefined,
+        blankContents,
+      );
+
+      const lastElement = htmlElement.getLastElement();
+      const previousElement = htmlElement.getSecondToTheLastElement();
+      const lastQueryElement = document.querySelector(`${lastElement}`);
+      const previousQueryElement = document.querySelector(`${previousElement}`);
+
+      previousQueryElement.classList.toggle('hide');
+      lastQueryElement.classList.toggle('hide');
+
+      // remove this project, remove it's content container, derefer project item from Projects,
+      // update localstorage after projects item has been derefered
+      li.remove();
+      projectContent.remove();
+      projectLists.dereferProjectItem(dataKey);
+      storingProjectsInLocalStorage.projectStoringInLocalStorage(projectLists.revealProjects());
+    });
 
     elements.forEach((element) => {
       element.setAttribute(
