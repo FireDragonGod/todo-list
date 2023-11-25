071e0db7e7e1120a6fbfada8c1637e0675e569ba
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-11-25 15:59:33 +0800

    Make Style consistent to edit_todo js file
    Change:
      remove todoLists module

--- src/js/create_html_files/edit_todo.js
+++ src/js/create_html_files/edit_todo.js
@@ -1,6 +1,9 @@
-import close from '../../assets/img/close_FILL0_wght400_GRAD0_opsz24_copy.svg'
-import { updateTodoValues, todoValue } from '../create_todo/update_todo';
-import { matchValue, todoLists } from '../create_todo/todo_create';
+// images
+import close from '../../assets/img/close_FILL0_wght400_GRAD0_opsz24_copy.svg';
+
+// logic
+import { updatingTodoValues, todoValue } from '../create_todo/update_todo';
+import { matchValue } from '../create_todo/todo_create';
 
 class EditTodo {
 
@@ -46,68 +49,202 @@ class EditTodo {
     closeImg.src = close;
   
     labelTitle.textContent = 'Title: ';
-    labelTitle.setAttribute('for', 'task_name');
-    titleInput.setAttribute('type', 'text');
-    titleInput.setAttribute('name', 'task_name');
-    titleInput.setAttribute('id', 'task_name');
-    titleInput.setAttribute('placeholder', 'todo-list');
-    titleInput.setAttribute('minlength', '4');
-    titleInput.setAttribute('maxlength', '64');
-    titleInput.setAttribute('required', '');
-    titleInput.setAttribute('value',`${title}`);
+    labelTitle.setAttribute(
+      'for', 
+      'task_name'
+    );
+    titleInput.setAttribute(
+      'type', 
+      'text'
+    );
+    titleInput.setAttribute(
+      'name', 
+      'task_name'
+    );
+    titleInput.setAttribute(
+      'id', 
+      'task_name'
+    );
+    titleInput.setAttribute(
+      'placeholder', 
+      'todo-list'
+    );
+    titleInput.setAttribute(
+      'minlength', 
+      '4'
+    );
+    titleInput.setAttribute(
+      'maxlength', 
+      '64'
+    );
+    titleInput.setAttribute(
+      'required', 
+      ''
+    );
+    titleInput.setAttribute(
+      'value', 
+      `${title}`
+    );
 
     labelDescription.textContent = 'Description: '
-    labelDescription.setAttribute('for', 'task_description');
-    descriptionInput.setAttribute('type', 'text');
-    descriptionInput.setAttribute('name', 'task_description');
-    descriptionInput.setAttribute('minlength', '');
-    descriptionInput.setAttribute('maxlength', '248');
-    descriptionInput.setAttribute('id', 'task_description');
-    descriptionInput.setAttribute('placeholder', 'Provide more info about the task')
+    labelDescription.setAttribute(
+      'for', 
+      'task_description'
+    );
+    descriptionInput.setAttribute(
+      'type', 
+      'text'
+    );
+    descriptionInput.setAttribute(
+      'name', 
+      'task_description'
+    );
+    descriptionInput.setAttribute(
+      'minlength', 
+      ''
+    );
+    descriptionInput.setAttribute(
+      'maxlength', 
+      '248'
+    );
+    descriptionInput.setAttribute(
+      'id', 
+      'task_description'
+    );
+    descriptionInput.setAttribute(
+      'placeholder', 
+      'Provide more info about the task'
+    );
 
     descriptionInput.value = `${description}`;
 
     priorityInput.textContent = 'Priority Level: ';
 
     labelLow.textContent = 'Low Priority'
-    labelLow.setAttribute('for', 'low_priority');
-    lowInput.setAttribute('type', 'radio');
-    lowInput.setAttribute('id', 'low_priority');
-    lowInput.setAttribute('name', 'priority_level');
-    lowInput.setAttribute('value', 'low_priority');
+    labelLow.setAttribute(
+      'for', 
+      'low_priority'
+    );
+    lowInput.setAttribute(
+      'type', 
+      'radio'
+    );
+    lowInput.setAttribute(
+      'id', 
+      'low_priority'
+    );
+    lowInput.setAttribute(
+      'name', 
+      'priority_level'
+    );
+    lowInput.setAttribute(
+      'value', 
+      'low_priority'
+    );
 
     labelMedium.textContent = 'Medium Priority';
-    labelMedium.setAttribute('for', 'medium_priority');
-    mediumInput.setAttribute('type', 'radio');
-    mediumInput.setAttribute('id', 'medium_priority');
-    mediumInput.setAttribute('name', 'priority_level');
-    mediumInput.setAttribute('value', 'medium_priority');
+    labelMedium.setAttribute(
+      'for', 
+      'medium_priority'
+    );
+    mediumInput.setAttribute(
+      'type', 
+      'radio'
+    );
+    mediumInput.setAttribute(
+      'id', 
+      'medium_priority'
+    );
+    mediumInput.setAttribute(
+      'name', 
+      'priority_level'
+    );
+    mediumInput.setAttribute(
+      'value', 
+      'medium_priority'
+    );
 
     labelHigh.textContent = 'High Priority';
-    labelHigh.setAttribute('for', 'high_priority');
-    highInput.setAttribute('type', 'radio');
-    highInput.setAttribute('id', 'high_priority');
-    highInput.setAttribute('name', 'priority_level');
-    highInput.setAttribute('value', 'high_priority');
+    labelHigh.setAttribute(
+      'for', 
+      'high_priority'
+    );
+    highInput.setAttribute(
+      'type', 
+      'radio'
+    );
+    highInput.setAttribute(
+      'id', 
+      'high_priority'
+    );
+    highInput.setAttribute(
+      'name', 
+      'priority_level'
+    );
+    highInput.setAttribute(
+      'value', 
+      'high_priority'
+    );
 
     labelDueDate.textContent = 'Due Date: ';
-    labelDueDate.setAttribute('for', 'due_date');
-    dueDateInput.setAttribute('type', 'date');
-    dueDateInput.setAttribute('name', 'due_date');
-    dueDateInput.setAttribute('id', 'due_date');
-    dueDateInput.setAttribute('min', ``);
-    dueDateInput.setAttribute('max', '2100-12-31');
-    dueDateInput.setAttribute('value', `${dueDate}`);
+    labelDueDate.setAttribute(
+      'for', 
+      'due_date'
+    );
+    dueDateInput.setAttribute(
+      'type', 
+      'date'
+    );
+    dueDateInput.setAttribute(
+      'name', 
+      'due_date'
+    );
+    dueDateInput.setAttribute(
+      'id', 
+      'due_date'
+    );
+    dueDateInput.setAttribute(
+      'min', ``
+    );
+    dueDateInput.setAttribute(
+      'max', 
+      '2100-12-31'
+    );
+    dueDateInput.setAttribute(
+      'value', 
+      `${dueDate}`
+    );
 
-    labelSubmit.setAttribute('for', 'submit_todo');
     submitInput.textContent = 'Change'
-    submitInput.setAttribute('id', 'submit_todo');
-    submitInput.setAttribute('name', 'submit_todo');
-    submitInput.setAttribute('type', 'submit');
+    labelSubmit.setAttribute(
+      'for', 
+      'submit_todo'
+    );
+    submitInput.setAttribute(
+      'id', 
+      'submit_todo'
+    );
+    submitInput.setAttribute(
+      'name', 
+      'submit_todo'
+    );
+    submitInput.setAttribute(
+      'type', 
+      'submit'
+    );
 
-    lowInput.checked = matchValue.match('low_priority', priority);
-    mediumInput.checked = matchValue.match('medium_priority', priority);
-    highInput.checked = matchValue.match('high_priority', priority);
+    lowInput.checked = matchValue.match(
+      'low_priority', 
+      priority,
+    );
+    mediumInput.checked = matchValue.match(
+      'medium_priority', 
+      priority,
+    );
+    highInput.checked = matchValue.match(
+      'high_priority', 
+      priority
+    );
 
     let priorityLevelStore = `${priority}`;
 
@@ -119,53 +256,49 @@ class EditTodo {
 
     radios.forEach((element) => {
     
-      element.addEventListener(
-        'change',
-        (event) => {
-          priorityLevelStore = event.target.value;
-        }
-      )
+      element.addEventListener('change', (event) => {
+        priorityLevelStore = event.target.value;
+      })
     });
 
-    submitInput.addEventListener(
-      'click',
-      (event) => {
-        const todoItemTitle = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(2) > h2`);
-        const todoItemPriorityColor = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(3)`);
-        const todoItemDueDate = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(4) > p`);
-        const titleInputValue = titleInput.value;
-        const descriptionInputValue = descriptionInput.value;
-        const lowInputRadio = lowInput;
-        const mediumInputRadio = mediumInput;
-        const highInputRadio = highInput;
-        const dueDateInputValue = dueDateInput.value;
+    submitInput.addEventListener('click', (event) => {
 
-        todoValue.todoValueSet(
-          titleInputValue,
-          descriptionInputValue,
-          priorityLevelStore,
-          dueDateInputValue,
-          dataKey
-        );
+      const todoItemTitle = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(2) > h2`);
+      const todoItemPriorityColor = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(3)`);
+      const todoItemDueDate = document.querySelector(`div[data-todo-item='${dataKey}'] > div:nth-child(4) > p`);
+      const titleInputValue = titleInput.value;
+      const descriptionInputValue = descriptionInput.value;
+      const lowInputRadio = lowInput;
+      const mediumInputRadio = mediumInput;
+      const highInputRadio = highInput;
+      const dueDateInputValue = dueDateInput.value;
 
-        updateTodoValues.conditionValues(
-          event,
-          titleInputValue,
-          dataKey,
-          priorityLevelStore,
-          todoItemTitle,
-          todoItemPriorityColor,
-          todoItemDueDate,
-          dialog,
-          titleInputValue,
-          descriptionInputValue,
-          lowInputRadio,
-          mediumInputRadio,
-          highInputRadio,
-          dueDateInputValue,
-        );
-      }
-    );
+      todoValue.todoValueSet(
+        titleInputValue,
+        descriptionInputValue,
+        priorityLevelStore,
+        dueDateInputValue,
+        dataKey,
+      );
+
+      updatingTodoValues.conditionValuesBeforeMakingChangesToThem(
+        event,
+        titleInputValue,
+        dataKey,
+        priorityLevelStore,
+        todoItemTitle,
+        todoItemPriorityColor,
+        todoItemDueDate,
+        dialog,
+        titleInputValue,
+        descriptionInputValue,
+        lowInputRadio,
+        mediumInputRadio,
+        highInputRadio,
+        dueDateInputValue,
+      );
+
+    });
 
     form.setAttribute(
       'data-todo-edit',
@@ -185,68 +318,63 @@ class EditTodo {
       parent, 
       children
     ) {
-      children.forEach(child => {
+      children.forEach((child) => {
         parent.appendChild(child)
       })
     };
 
-    closeImg.addEventListener(
-      'click',
-      (event) => {
-        dialog.close();
-      }
-    );
+    closeImg.addEventListener('click', () => {
+      dialog.close();
+    });
     
-    appendMultipleChildren(
-      ul, 
-      [
-        liImg,
-        liTitle,
-        liDescription,
-        liPriority,
-        liLow,
-        liMedium,
-        liHigh,
-        liDueDate,
-        liSubmit,
-      ]
-    );
+    appendMultipleChildren(ul, [
+      liImg,
+      liTitle,
+      liDescription,
+      liPriority,
+      liLow,
+      liMedium,
+      liHigh,
+      liDueDate,
+      liSubmit,
+    ]);
 
-    appendMultipleChildren(
-      liTitle, 
-      [
-        labelTitle, 
-        titleInput,
-      ]
-    );
-    appendMultipleChildren(
-      liDescription, 
-      [
-        labelDescription, 
-        descriptionInput,
-      ]
-    );
+    appendMultipleChildren(liTitle, [
+      labelTitle, 
+      titleInput,
+    ]);
     
-    appendMultipleChildren(
-      liDueDate, 
-      [
-        labelDueDate, 
-        dueDateInput,
-      ]
-    );
+    appendMultipleChildren(liDescription, [
+      labelDescription, 
+      descriptionInput,
+    ]);
     
-    appendMultipleChildren(liPriority, [priorityInput]);
-    appendMultipleChildren(liLow, [lowInput, labelLow]);
-    appendMultipleChildren(liMedium, [mediumInput, labelMedium, ])
-    appendMultipleChildren(liHigh, [highInput, labelHigh, ])
+    appendMultipleChildren(liDueDate, [
+      labelDueDate, 
+      dueDateInput,
+    ]);
+    
+    appendMultipleChildren(liPriority, [
+      priorityInput
+    ]);
+    appendMultipleChildren(liLow, [
+      lowInput, 
+      labelLow,
+    ]);
+    appendMultipleChildren(liMedium, [
+      mediumInput, 
+      labelMedium, 
+    ])
+    appendMultipleChildren(liHigh, [
+      highInput, 
+      labelHigh, 
+    ])
 
-    appendMultipleChildren(
-      liSubmit, 
-      [
-        labelSubmit, 
-        submitInput,
-      ]
-    );
+    appendMultipleChildren(liSubmit, [
+      labelSubmit, 
+      submitInput,
+    ]);
+    
     return dialog;
   };
 };
