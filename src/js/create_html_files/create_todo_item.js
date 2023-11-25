61b1989035b266509e40aedb15a064ff9deafce3
murasakino <120464393+FireDragonGod@users.noreply.github.com>
2023-11-25 15:55:49 +0800

    Make style consistent to create_todo_items
    Change:
      Send two copy of priority value to update_todo js file array called
      classListsColorPriority to toggle color on load or after creating a
      todo item.

--- src/js/create_html_files/create_todo_item.js
+++ src/js/create_html_files/create_todo_item.js
@@ -1,7 +1,13 @@
 import remove from '../../assets/img/remove_FILL0_wght400_GRAD0_opsz24.svg';
 import edit from '../../assets/img/edit_FILL0_wght400_GRAD0_opsz24.svg';
+
+// application
 import { editTodoItem } from './edit_todo';
-import { todoLists } from '../create_todo/todo_create';
+
+// logic
+import { dates, todoLists } from '../create_todo/todo_create';
+import { updatingTodoValues } from '../create_todo/update_todo';
+
 
 function appendMoreChildren(
   parent, 
@@ -21,6 +27,7 @@ class TodoItem {
     dueDate,
     description,
     priorityLevel,
+    ProjectThisTodoIsCreated,
   ) {
     const todoItemWrapper = document.createElement('div');
     const form = document.createElement('form');
@@ -38,22 +45,17 @@ class TodoItem {
     const paraDate = document.createElement('p');
 
     divPriority.classList.add(`${colorCoding}`);
+
     todoItemWrapper.setAttribute(
       'class',
-      'todo-item'
-    )
+      'todo-item',
+    );
 
     let removeImg = new Image();
     let editContent = new Image();
 
     h2.textContent = `${title}`;
-    paraDate.textContent = `${dueDate}`;
-
-    const datesForForm = {
-      dueDateMm: dueDate.slice(0, 2),
-      dueDateDd: dueDate.slice(3, 5),
-      dueDateYyyy: dueDate.slice(6, 10),
-    };
+    paraDate.textContent = `${dates.formatDate(dueDate)}`;
 
     removeImg.setAttribute(
       'alt',
@@ -73,6 +75,11 @@ class TodoItem {
       `${dataTodoItem}`,
     );
 
+    todoItemWrapper.setAttribute(
+      'data-project-this-todo-was-created',
+      `${ProjectThisTodoIsCreated}`
+    );
+
     form.setAttribute(
       'method',
       'get',
@@ -88,20 +95,18 @@ class TodoItem {
       'checkList',
     );
 
-    removeImg.addEventListener(
-      'click',
-      (event) => {
-        todoLists.dereferTodoListsItem(dataTodoItem);
-        todoItemWrapper.remove();
-      }
-    )
+    removeImg.addEventListener('click', () => {
+      todoLists.dereferTodoListsItem(dataTodoItem);
+      todoItemWrapper.remove();
+      // After this todoItem has been derefered
+      // set the todoLists to localstorage so the todoLists is coming together with derefered item
+      // application > localstorage = todo: [removedItem = null, { otherTodoList }] like so
+      localStorage.setItem('Todo', JSON.stringify(todoLists.revealTodoLists()))
+    });
 
-    check.addEventListener(
-      'click',
-      (event) => {
-        event.target.checked ? h2.classList.toggle('strikethrough') : h2.classList.toggle('strikethrough');
-      }
-    );
+    check.addEventListener('click', (event) => {
+      event.target.checked ? h2.classList.toggle('strikethrough') : h2.classList.toggle('strikethrough');
+    });
 
     check.setAttribute(
       'type',
@@ -126,42 +131,37 @@ class TodoItem {
       title,
       description,
       priorityLevel,
-      `${datesForForm.dueDateYyyy}-${datesForForm.dueDateMm}-${datesForForm.dueDateDd}`,
+      dueDate,
     ));
 
-    editContent.addEventListener(
-      'click',
-      (event) => {
-        const modal = document.querySelector(`dialog[data-todo-edit="${dataTodoItem}"]`);
-        modal.showModal();
-      }
-    )
+    // send PriorityClasListItem to toggle them on load 
+    updatingTodoValues.addPriorityClassListsItem(priorityLevel);
+    updatingTodoValues.addPriorityClassListsItem(priorityLevel);
+
+    editContent.addEventListener('click', () => {
+      const modal = document.querySelector(`dialog[data-todo-edit="${dataTodoItem}"]`);
+      modal.showModal();
+    });
 
     form.appendChild(onles);
     onles.appendChild(li);
 
-    appendMoreChildren(
-      li, 
-      [
-        check,
-        checkLabel,
-      ]
-    );
+    appendMoreChildren(li, [
+      check,
+      checkLabel,
+    ]);
 
     divTitle.appendChild(h2);
-    divDueDate.appendChild(paraDate)
+    divDueDate.appendChild(paraDate);
 
-    appendMoreChildren(
-      todoItemWrapper, 
-      [
-        form, 
-        divTitle,
-        divPriority,
-        divDueDate,
-        divRemove,
-        divEdit,
-      ]
-    );
+    appendMoreChildren(todoItemWrapper, [
+      form, 
+      divTitle,
+      divPriority,
+      divDueDate,
+      divRemove,
+      divEdit,
+    ]);
 
     todoItemWrapper.classList.add('todo_item');
 
